28baab7f83ab7a5b360ef4240e1802c45aecb68b
mtclark0518 <mtclark0518@gmail.com>
2017-07-28 15:57:12 -0600

    brings in bootstrap

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,12 @@
 
-
+console.log("hello world");
+// 	var ajax = $.get("https://na1.api.riotgames.com/lol/static-data/v3/champions?api_key=" + apiKey);
 
 
 // $(document).ready(function(){
 
 
 
-// 	var ajax = $.get("https://na1.api.riotgames.com/lol/static-data/v3/champions?api_key=" + apiKey);
 // 	ajax.done(function(data){
 // 		console.log(data);
 // 	});
